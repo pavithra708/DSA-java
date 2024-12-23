9e5f3d203a916371dfb62a41aac19a8a84ade96b
pavithra708 <pavithrarevankar70@gmail.com>
2024-12-23 19:57:15 +0530

    Create Largest odd number in a string.java

---
+++ Leetcode/Easy/Largest odd number in a string.java
@@ -0,0 +1,10 @@
+	public class{
+	public static void main(String[] args){
+	String str="52";
+	for(int i=str.length()-1;i>=0;i--){
+	If(Character.getNumericValue(str.charAt(i)%2!=0){
+	return str.substring(0,i+1);
+	  }
+   }
+  }
+}
